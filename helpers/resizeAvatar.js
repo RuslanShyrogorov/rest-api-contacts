1c07fbb9967710c3c1ea4de19c40b9bab1dbcabd
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2023-01-27 22:05:29 +0200

    add trycatch

--- helpers/resizeAvatar.js
+++ helpers/resizeAvatar.js
@@ -1,9 +1,13 @@
 const Jimp = require("jimp");
 
 const resizeAvatar = async (avatar) => {
-  const resizeAvatar = await Jimp.read(avatar);
-  resizeAvatar.resize(250, 250);
-  resizeAvatar.write(avatar);
+  try {
+    const resizeAvatar = await Jimp.read(avatar);
+    resizeAvatar.resize(250, 250);
+    resizeAvatar.write(avatar);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 module.exports = resizeAvatar;
